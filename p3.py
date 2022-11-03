4f584f8ca5758286028daef68caf8c2698377401
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-11-03 10:38:28 +0300

    3

--- p3.py
+++ p3.py
@@ -1,10 +1,12 @@
 def main(list1):
-  i=0
-  a=len(list1)
-  s=0
-  while i<a:
-    if list1[i]%2==0:
-        s+=list1[i]
-    i+=1
-  return s
-print(main([5,3,2,8,5]))
+        
+
+    i=0
+    a=len(list1)
+    s=0
+    while i<a:
+        if i%2==1:
+           s+=list1[i]
+        i+=1
+    return s
+print(main([7,8,3,5,2])) 
